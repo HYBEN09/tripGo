9e91c0e857e70aad0e134703627818e34da61781
hyebeen byun <hyebeen2658@naver.com>
2023-06-20 17:43:14 +0900

    [feat] sticky header 구현

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-import { setupVideoControls } from "./public/scripts/index.js";
+import { setupVideoControls, getNode } from "./public/scripts/index.js";
 
+const header = getNode("header");
 // 홈 비디오 동작 제어
 setupVideoControls();
+
+// sticky header
+
+function handleScroll() {
+  header.classList.toggle("sticky", window.scrollY > 0);
+}
+
+window.addEventListener("scroll", handleScroll);
