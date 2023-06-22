0961f37b4c7499d9a7fafa477b17bc658d399ed8
hyebeen byun <hyebeen2658@naver.com>
2023-06-22 17:24:34 +0900

    [feat] swiper 이용하여 banner 구현

--- main.js
+++ main.js
@@ -100,3 +100,37 @@ navLinks.forEach((link) => {
     menuIcon.classList.add("bx-menu");
   });
 });
+
+const data = [
+  {
+    id: 1,
+    src: "./assets/img/banner/banner1.svg",
+    alt: "전국 호텔 안심특가 광고",
+  },
+  {
+    id: 2,
+    src: "./assets/img/banner/banner2.svg",
+    alt: "숙박 세일 페스타 광고",
+  },
+  {
+    id: 3,
+    src: "./assets/img/banner/banner3.svg",
+    alt: "인기 급상승 갓성비 호텔 광고",
+  },
+  {
+    id: 4,
+    src: "./assets/img/banner/banner4.svg",
+    alt: "요즘 호캉스 패키지 모음 광고",
+  },
+];
+
+const advertisementContainer = getNode(".advertisement .swiper-wrapper");
+
+data.forEach((item) => {
+  const slideHTML = `
+    <div class="swiper-slide">
+      <img src="${item.src}" alt="${item.alt}">
+    </div>
+  `;
+  advertisementContainer.insertAdjacentHTML("beforeend", slideHTML);
+});
