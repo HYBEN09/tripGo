93c8647f7109ad8472e915a561b7ebcb2cd38b5c
hyebeen byun <hyebeen2658@naver.com>
2023-06-22 18:31:35 +0900

    [feat] swiper 수정

--- main.js
+++ main.js
@@ -104,21 +104,25 @@ navLinks.forEach((link) => {
 const data = [
   {
     id: 1,
+    link: "https://www.tripbtoz.com/md/hb2023",
     src: "./assets/img/banner/banner1.svg",
     alt: "전국 호텔 안심특가 광고",
   },
   {
     id: 2,
+    link: "https://www.tripbtoz.com/event/stayfesta2023",
     src: "./assets/img/banner/banner2.svg",
     alt: "숙박 세일 페스타 광고",
   },
   {
     id: 3,
+    link: "https://www.tripbtoz.com/md/star123",
     src: "./assets/img/banner/banner3.svg",
     alt: "인기 급상승 갓성비 호텔 광고",
   },
   {
     id: 4,
+    link: "https://www.tripbtoz.com/md/hotelpackage23",
     src: "./assets/img/banner/banner4.svg",
     alt: "요즘 호캉스 패키지 모음 광고",
   },
@@ -127,10 +131,14 @@ const data = [
 const advertisementContainer = getNode(".advertisement .swiper-wrapper");
 
 data.forEach((item) => {
-  const slideHTML = `
-    <div class="swiper-slide">
+  const slideHTML = /*html*/ `
+    <ul class="swiper-slide">
+      <li>
+      <a href="${item.link}" target="_blank">
       <img src="${item.src}" alt="${item.alt}">
-    </div>
+      </a>
+      </li>
+    </ul>
   `;
   advertisementContainer.insertAdjacentHTML("beforeend", slideHTML);
 });
