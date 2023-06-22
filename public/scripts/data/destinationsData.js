110295a2427211389ca554bff075f4bf671673b4
hyebeen byun <hyebeen2658@naver.com>
2023-06-22 14:24:50 +0900

    [chore] 이미지 경로 재설정

--- public/scripts/data/destinationsData.js
+++ public/scripts/data/destinationsData.js
@@ -1,44 +1,38 @@
 export const destinations = [
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1609944433409-81bda5323abc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVCJUE3JTg4JUVDJUI2JTk0JTIwJUVEJTk0JUJEJUVDJUI2JTk0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
-    altText: '마추픽추',
-    title: 'Machu Picchu',
-    country: 'PERU',
+    imageSrc: "./assets/img/destinations/peru.png",
+    altText: "마추픽추",
+    title: "Machu Picchu",
+    country: "PERU",
   },
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1558642084-fd07fae5282e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fCVFQiVCMCU5NCVFQiVBMSU5QyVFQyU4NSU4MCVFQiVBMSU5QyVFQiU4MiU5OHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
-    altText: '바르셀로나',
-    title: 'Barcelona',
-    country: 'Spain',
+    imageSrc: "./assets/img/destinations/spain.png",
+    altText: "바르셀로나",
+    title: "Barcelona",
+    country: "Spain",
   },
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1537953773345-d172ccf13cf1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8JUVCJUIwJTlDJUVCJUE2JUFDfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
-    altText: '발리',
-    title: 'Bali',
-    country: 'Indonesia',
+    imageSrc: "./assets/img/destinations/bali.png",
+    altText: "발리",
+    title: "Bali",
+    country: "Indonesia",
   },
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1568402102990-bc541580b59f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhbmN1bnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60',
-    altText: '마야 문명',
-    title: 'Cancun',
-    country: 'Mexico',
+    imageSrc: "./assets/img/destinations/mexico.png",
+    altText: "마야 문명",
+    title: "Cancun",
+    country: "Mexico",
   },
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1603199766980-fdd4ac568a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJvbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60',
-    altText: '콜로세움',
-    title: 'Rome',
-    country: 'Italy',
+    imageSrc: "./assets/img/destinations/italy.png",
+    altText: "콜로세움",
+    title: "Rome",
+    country: "Italy",
   },
   {
-    imageSrc:
-      'https://images.unsplash.com/photo-1473398643778-d68e48a374c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHBhcml8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60',
-    altText: '에펠탑',
-    title: 'Paris',
-    country: 'France',
+    imageSrc: "./assets/img/destinations/france.png",
+    altText: "에펠탑",
+    title: "Paris",
+    country: "France",
   },
 ];
